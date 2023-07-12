ab0d7d0cd48e47bdd6c30ba9563054b0d1faf84a
lafeillou <lafeillou@126.com>
2023-07-12 15:59:17 +0800

    指南-管理输出

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,36 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js',
     },
-    mode: 'development'
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type:'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: '管理输出'
+        })
+    ]
 };
